bbf5bf7582a27e433352452618470b97493d062c
belvitabiscuits <li.kehl@yale.edu>
2015-11-09 13:28:06 +0000

    Unordered list of events with pertinent information

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,16 @@
 'use strict';
 
+var events = require('../models/events');
+
+
 /**
  * Controller that renders our index (home) page.
  */
 function index (request, response) {
   var contextData = {
     'title': 'Team Young Frost',
-    'tagline': 'You are doomed (just kidding).'
+    'tagline': 'You are doomed (just kidding).',
+    'events': events.all
   };
   response.render('index.html', contextData);
 }
